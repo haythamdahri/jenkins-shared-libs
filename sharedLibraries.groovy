60e57df9d69c5bcabecdd32187bc6c2080fad1bc
haytham_dahri <haytham_dahri@carrefour.com>
2021-11-28 16:35:16 +0100

    Remove unecessary files

--- sharedLibraries.groovy
+++
@@ -1,6 +0,0 @@
-
-// Execute ansible playbooks in a file with an inventory definitions
-def executePlaybook(String playbooksFilename, String inventoryFilename) {
- echo "Running playbook ${playbooksFilename} with inventory file ${inventoryFilename}"
- sh "ansible-playbook ${playbooksFilename} -i ${inventoryFilename}"
-}
